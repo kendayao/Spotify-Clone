6d25a8bf3c50d113498c4cbd57faec7abd6cffba
Kaeneth Dayao <kendayao@gmail.com>
2020-12-10 20:39:31 -0800

    spotfy auth setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import Login from './components/login'
+import React, {useState, useEffect} from "react";
+import Login from './components/login/Login';
+import {getTokenFromUrl} from './utils/spotify';
 import './App.css';
 
 function App() {
+
+  const[token, setToken]=useState(null);
+
+    useEffect(()=>{
+      const hash=getTokenFromUrl();
+      window.location.hash="";
+      const _token=hash.access_token;
+
+      if(_token){
+        setToken(_token)
+      }
+
+
+    },[])
+  
   return (
     <div className="app">
       <Login />
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
+import { loginUrl } from '../../utils/spotify';
+import './Login.css';
 
 function Login() {
     return (
         <div className="login">
-            <h1>Login Page</h1>
+            <img src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg' alt="spotify logo"/>
+            <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
